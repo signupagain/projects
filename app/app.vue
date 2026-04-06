844c8fca5ee19e3173a0d1a82064aa467b623cf2
jerry <s1062940@yahoo.com>
2026-04-06 19:15:03 +0800

    feat(intro): add default layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+	useHead({
+		htmlAttrs: {
+			lang: 'zh-Hant-TW',
+		},
+	})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<UApp>
+		<NuxtRouteAnnouncer />
+		<NuxtLayout>
+			<UMain class="relative">
+				<NuxtPage />
+			</UMain>
+		</NuxtLayout>
+	</UApp>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<UContainer class="border-default pt-10 sm:border-x">
+			<slot />
+		</UContainer>
+	</div>
+</template>
+
+<style lang="scss"></style>
