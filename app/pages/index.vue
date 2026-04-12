4c8edb8fbb8d159b09e7be4f102a16731822e032
jerry <s1062940@yahoo.com>
2026-04-12 19:09:34 +0800

    feat(intro): add index page

---
+++ app/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+	const { data: page } = await useFetch('/api')
+
+	if (!page.value) {
+		const appConfig = useAppConfig()
+		throw createError(appConfig.error.notFound)
+	}
+
+	useSeoMeta({
+		title: page.value?.seo.title || page.value?.title,
+		ogTitle: page.value?.seo.title || page.value?.title,
+		description: page.value?.seo.description || page.value?.description,
+		ogDescription: page.value?.seo.description || page.value?.description,
+	})
+</script>
+
+<template>
+	<UPage v-if="page" as="main">
+		<pre>{{ page }}</pre>
+	</UPage>
+</template>
+
+<style lang="scss"></style>
