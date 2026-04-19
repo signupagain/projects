085da1a05760b581ce836d8902698b50dabe3070
jerry <s1062940@yahoo.com>
2026-04-19 16:50:26 +0800

    feat(error): add error handler

---
+++ app/error.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+	import type { NuxtError } from '#app'
+
+	const { error } = defineProps<{
+		error: NuxtError
+	}>()
+
+	useHead({
+		htmlAttrs: {
+			lang: 'zh-Hant-TW',
+		},
+	})
+
+	useSeoMeta({
+		title: error.status + ' - ' + error.message,
+	})
+
+	const { navLinks, credits, links: footerLinks } = useAppConfig()
+</script>
+
+<template>
+	<div>
+		<AppHeader :links="navLinks" />
+
+		<UMain>
+			<UContainer>
+				<UPage>
+					<UError :error="error" />
+				</UPage>
+			</UContainer>
+		</UMain>
+
+		<AppFooter :credits :links="footerLinks" />
+	</div>
+</template>
+
+<style lang="scss"></style>
