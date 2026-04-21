4ba7868494cd2c92edd442508392a2f49dca2a78
jerry <s1062940@yahoo.com>
2026-04-22 01:23:55 +0800

    chore(eslint): integrate eslint-config-prettier for improved code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import pluginPrettier from 'eslint-plugin-prettier/recommended'
 
 export default withNuxt(
-  // Your custom configs here
+	// Your custom configs here
+	pluginPrettier,
 )
