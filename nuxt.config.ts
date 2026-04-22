83d933a06e5735847d5892be02b190ac401533f2
jerry <s1062940@yahoo.com>
2026-04-22 16:40:22 +0800

    style(font): add Noto Sans TC and Noto Serif SC fonts to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,4 +35,17 @@ export default defineNuxtConfig({
 	},
 
 	css: ['~/assets/css/main.css'],
+
+	app: {
+		head: {
+			link: [
+				{ rel: 'preconnect', href: 'https://googleapis.com' },
+				{ rel: 'preconnect', href: 'https://gstatic.com', crossorigin: '' },
+				{
+					rel: 'stylesheet',
+					href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Noto+Serif+SC:wght@200..900&display=swap',
+				},
+			],
+		},
+	},
 })
