7101dad1b47c4b2660967d2d7d9d87e21dd0d14f
jerry <s1062940@yahoo.com>
2026-04-30 18:57:26 +0800

    feat(gallery): add the header of gallery pages
    1. add GalleryDefault layout
    2. add Header and Search components
    3. add useSearchQuery and useSearchRecords composables
    4. add test for useSearchRecords composable
    5. define the route for gallery page

---
+++ layers/gallery/app/pages/gallery/[[id]].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+	export type GalleryPageQuery = {
+		search?: string
+	}
+
+	definePageMeta({
+		layout: 'gallery-default',
+	})
+</script>
+
+<template>
+	<main></main>
+</template>
+
+<style lang="scss"></style>
