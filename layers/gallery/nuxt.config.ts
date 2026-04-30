abf7e3ec2a6f1ae43bd1f85b75016f9cad78804e
jerry <s1062940@yahoo.com>
2026-04-30 19:28:30 +0800

    feat(gallery): initialize gallery layer

---
+++ layers/gallery/nuxt.config.ts
@@ -0,0 +1,56 @@
+import fs from 'fs'
+import path from 'path'
+
+export default defineNuxtConfig({
+	extends: ['../colorModeBtn'],
+
+	runtimeConfig: {
+		pexelsURL: process.env.PEXELS_URL,
+		pexelsToken: process.env.PEXELS_TOKEN,
+	},
+
+	image: {
+		domains: ['images.pexels.com'],
+		alias: {
+			pexels: 'https://images.pexels.com/photos',
+		},
+	},
+
+	modules: [
+		'@nuxt/image',
+		'@nuxt/test-utils/module',
+		'@nuxt/ui',
+		'@vueuse/nuxt',
+		'@peterbud/nuxt-query',
+		'@vueuse/nuxt',
+	],
+
+	nuxtQuery: {
+		queryClientOptions: {
+			defaultOptions: {
+				queries: {
+					staleTime: 1000 * 60 * 5, // 5 minutes
+				},
+			},
+		},
+	},
+
+	experimental: {
+		typedPages: true,
+	},
+
+	$development: {
+		devServer: {
+			https: {
+				key: fs
+					.readFileSync(path.resolve(process.cwd(), 'certs/localhost-key.pem'))
+					.toString(),
+				cert: fs
+					.readFileSync(path.resolve(process.cwd(), 'certs/localhost.pem'))
+					.toString(),
+			},
+			host: '0.0.0.0',
+			port: 3000,
+		},
+	},
+})
