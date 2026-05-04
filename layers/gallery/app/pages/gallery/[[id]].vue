9e46d5aad93f24c4710af338d81f4b9b2b4feed4
jerry <s1062940@yahoo.com>
2026-05-04 16:01:16 +0800

    feat(gallery): add the title of gallery entry page

--- layers/gallery/app/pages/gallery/[[id]].vue
+++ layers/gallery/app/pages/gallery/[[id]].vue
@@ -5,11 +5,44 @@
 
 	definePageMeta({
 		layout: 'gallery-default',
+		validate(meta) {
+			const appConfig = useAppConfig()
+
+			if (meta.name !== 'gallery-id') return appConfig.error.notFound
+
+			return (
+				(meta.params?.id === undefined ?
+					true
+				:	typeof +meta.params.id === 'number') || appConfig.error.notFound
+			)
+		},
+	})
+
+	const { data: page } = await useFetch('/api/gallery')
+
+	if (!page.value) {
+		const appConfig = useAppConfig()
+		throw createError(appConfig.error.notFound)
+	}
+
+	useSeoMeta({
+		title: page.value?.seo.title || page.value?.title,
+		ogTitle: page.value?.seo.title || page.value?.title,
+		description: page.value?.seo.description || page.value?.description,
+		ogDescription: page.value?.seo.description || page.value?.description,
 	})
 </script>
 
 <template>
-	<main></main>
+	<main v-if="page">
+		<UPageHero
+			:title="page.title"
+			:description="page.description"
+			:ui="{
+				description: 'text-pretty',
+			}"
+		/>
+	</main>
 </template>
 
 <style lang="scss"></style>
