a3375c83aaab01a99f03467ee54292a04c85329d
jerry <s1062940@yahoo.com>
2026-05-04 16:01:16 +0800

    feat(gallery): add the title of gallery entry page

--- layers/gallery/app/pages/gallery/[[id]].vue
+++ layers/gallery/app/pages/gallery/[[id]].vue
@@ -5,11 +5,56 @@
 
 	definePageMeta({
 		layout: 'gallery-default',
+		validate(meta) {
+			if (meta.name !== 'gallery-id')
+				return {
+					statusCode: 404,
+					message: 'Page not found',
+					fatal: true,
+				}
+
+			return (
+				(meta.params?.id === undefined ?
+					true
+				:	typeof +meta.params.id === 'number') || {
+					statusCode: 404,
+					message: 'Page not found',
+					fatal: true,
+				}
+			)
+		},
+	})
+
+	const { data: page } = await useAsyncData('gallery-page', () =>
+		queryCollection('gallery').first(),
+	)
+
+	if (!page.value) {
+		throw createError({
+			statusCode: 404,
+			message: 'Page not found',
+			fatal: true,
+		})
+	}
+
+	useSeoMeta({
+		title: page.value?.seo.title || page.value?.title,
+		ogTitle: page.value?.seo.title || page.value?.title,
+		description: page.value?.seo.description || page.value?.description,
+		ogDescription: page.value?.seo.description || page.value?.description,
 	})
 </script>
 
 <template>
-	<div></div>
+	<div v-if="page">
+		<UPageHero
+			:title="page.title"
+			:description="page.description"
+			:ui="{
+				description: 'text-pretty',
+			}"
+		/>
+	</div>
 </template>
 
 <style lang="scss"></style>
