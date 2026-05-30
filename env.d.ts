c27dc9adba05a7ad08dfb6f990527e9e140b47a4
jerry <s1062940@yahoo.com>
2026-05-30 19:42:04 +0800

    feat(file-browser): add data-seed module

--- env.d.ts
+++ env.d.ts
@@ -9,5 +9,8 @@ declare namespace NodeJS {
 
 		/* Nelify */
 		readonly NETLIFY: string
+
+		/* File-Browser */
+		readonly FILE_SYSTEM_MAX_DEPTH: string
 	}
 }
---
+++ layers/fileBrowser/modules/data-seed/utils/fileTypes.ts
@@ -0,0 +1,91 @@
+export const fileTypes = [
+	{
+		icon: 'folder',
+		color: 'gray',
+		extensions: [{ name: '', label: 'Folder' }],
+	},
+	{
+		icon: 'file-archive',
+		color: 'gray',
+		extensions: [
+			{ name: 'rar', label: 'RAR Archive' },
+			{ name: 'zip', label: 'ZIP Archive' },
+		],
+	},
+	{
+		icon: 'audio-lines',
+		color: 'cyan',
+		extensions: [
+			{ name: 'm4a', label: 'M4A Audio' },
+			{ name: 'mp3', label: 'MP3 Audio' },
+			{ name: 'ogg', label: 'OGG Audio' },
+			{ name: 'wav', label: 'WAV Audio' },
+		],
+	},
+	{
+		icon: 'file-braces',
+		color: 'purple',
+		extensions: [
+			{ name: 'cpp', label: 'C++ Source' },
+			{ name: 'cs', label: 'C# Source' },
+			{ name: 'css', label: 'Cascading Stylesheet' },
+			{ name: 'js', label: 'JavaScript' },
+			{ name: 'ts', label: 'TypeScript' },
+			{ name: 'java', label: 'Java Source' },
+			{ name: 'json', label: 'JSON' },
+			{ name: 'scss', label: 'SCSS Source' },
+			{ name: 'py', label: 'Python Source' },
+			{ name: 'swift', label: 'Swift Source' },
+		],
+	},
+	{
+		icon: 'file-code',
+		color: 'orange',
+		extensions: [
+			{ name: 'html', label: 'HTML Document' },
+			{ name: 'md', label: 'Markdown Text File' },
+			{ name: 'pug', label: 'Pug Template' },
+			{ name: 'xml', label: 'XML Document' },
+		],
+	},
+	{
+		icon: 'file-text',
+		color: 'blue',
+		extensions: [
+			{ name: 'docx', label: 'Word Document' },
+			{ name: 'pdf', label: 'PDF Document' },
+			{ name: 'rtf', label: 'Rich Text Format' },
+			{ name: 'txt', label: 'Plain Text File' },
+		],
+	},
+	{
+		icon: 'image',
+		color: 'red',
+		extensions: [
+			{ name: 'gif', label: 'GIF Image' },
+			{ name: 'jpg', label: 'JPG Image' },
+			{ name: 'mermaid', label: 'Mermaid Diagram' },
+			{ name: 'png', label: 'PNG Image' },
+			{ name: 'svg', label: 'SVG Image' },
+			{ name: 'webp', label: 'WebP Image' },
+		],
+	},
+	{
+		icon: 'sheet',
+		color: 'green',
+		extensions: [
+			{ name: 'csv', label: 'Comma Separated Values' },
+			{ name: 'xlsx', label: 'Excel Spreadsheet' },
+		],
+	},
+	{
+		icon: 'video',
+		color: 'fuchsia',
+		extensions: [
+			{ name: 'mp4', label: 'MP4 Video' },
+			{ name: 'avi', label: 'AVI Video' },
+			{ name: 'mov', label: 'QuickTime Video' },
+			{ name: 'wmv', label: 'Windows Media Video' },
+		],
+	},
+]
