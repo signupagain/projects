76be90d5e63310f2b4bea9043065c27258e468e8
jerry <s1062940@yahoo.com>
2026-06-04 14:57:57 +0800

    feat(file-browser): add layout component

---
+++ layers/fileBrowser/app/layouts/FileBrowserDefault.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<UContainer
+			style="--ui-container: auto"
+			:ui="{
+				base: 'px-0!',
+			}"
+		>
+			<slot />
+		</UContainer>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
